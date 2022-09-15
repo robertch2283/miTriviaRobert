85150a59ff86db4ad46f118ecdf4bed63ab350d0
Robert Chambi <109832976+robertch2283@users.noreply.github.com>
2022-09-15 22:28:22 +0000

    Agregando retos de operadores matematicos al ejercicio

---
+++ retosOperadoresMatematicos.py
@@ -0,0 +1,20 @@
+a =int(input("Ingrese el primer numero: "))
+b = int(input("Ingrese el segundo numero: "))
+print((a+b)/2)
+
+#Reto de Operadores matematicos- Potencia
+print("\n Operadores matematicos- Potencia")
+c = int(input("Ingrese el numero: "))
+d = int(input("Ingrese  el la potencia a elevar: "))
+print(c**d)
+
+#Reto de Operadores matematicos- raiz cuadrada
+print("\n Operadores matematicos- raiz cuadrada")
+e = int(input("Ingrese el numero: "))
+print(e**(1/2))
+
+#Reto de Operadores matematicos-raiz cuadrada de la suma de la potencia de los dos numeros  
+print("\n Operadores matematicos- raiz cuadrada de la suma de la potencia de los dos numeros")
+f = int(input("Ingrese el primer numero: "))
+g = int(input("Ingrese el segundo numero: "))
+print((f**2+ g**2)**(1/2))
